05fdb5a474bf445cbeacd463bd09cf2783191ffa
Tammy Terstegge <s8taters@stud.uni-saarland.de>
2022-06-11 11:16:18 +0200

    fix map style

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
+import "../leaflet/leaflet.css";
 
 function Map() {
   const startPosition = [53.15, 10.25];
 
   return (
-    <div>
-      <MapContainer center={startPosition} zoom={8} scrollWheelZoom={false}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-      </MapContainer>
-    </div>
+    <MapContainer center={startPosition} zoom={8} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
   );
 }
 
